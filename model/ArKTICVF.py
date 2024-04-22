0cbd26312fe287b25e1071b250fad2e32b8d5ca5
Tanish Makadia <tanishmakadia@gmail.com>
2024-04-22 17:37:30 -0400

    feat: Begin implementing forward pass of main model

--- model/ArKTICVF.py
+++ model/ArKTICVF.py
@@ -8,7 +8,7 @@ class ArKTICVF(nn.Module):
 
         num_features = [192, 128, 64, 32]
         spatial_window_sizes = [(1, 8, 8), (1, 8, 8), (1, 8, 8), (1, 8, 8)] # For Sep-STS (Separated-Spatio-Temporal-SWIN) Encoder
-        num_heads = [2, 4, 8, 16]
+        num_heads = [2, 4, 8, 16] # For Multi-Head Attention
         self.joinType = joinType
         self.n_inputs = num_inputs
         self.n_outputs = num_outputs
@@ -20,8 +20,31 @@ class ArKTICVF(nn.Module):
         self.encoder = SepSTSEncoder(num_features, num_inputs, spatial_window_sizes, num_heads)
 
         self.decoder = nn.Sequential(
-            #define upSplit
+            upSplit(num_features[0], num_features[1]),
+            upSplit(num_features[1]*growth, num_features[2]),
+            upSplit(num_features[2]*growth, num_features[3]),
         )
+
+        def SmoothNet(in_channels, out_channels):
+            return nn.Sequential(
+                Conv3d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, batchnorm=False),
+                ResBlock(out_channels, kernel_size=3),
+            )
+        
+        num_features_out = 64
+        self.smooth1 = SmoothNet(num_features[1]*growth, num_features_out)
+        self.smooth2 = SmoothNet(num_features[2]*growth, num_features_out)
+        self.smooth3 = SmoothNet(num_features[3]*growth, num_features_out)
+
+        self.predict1 = # define SynBlock
+    
+    def forward(self, frames):
+        images = torch.stack(frames, dim=2)
+        _, _, _, H, W = images.shape
+
+        # Batch mean normalization works slightly better than global mean normalization (hence the repeated calls to .mean() below)
+        mean_ = images.mean(2, keepdim=True).mean(3, keepdim=True).mean(4, keepdim=True)
+        images = images - mean_
 
 
 class upSplit(nn.Module):
@@ -32,3 +55,17 @@ class upSplit(nn.Module):
     def forward(self, x, output_size):
         x = self.upconv(x, output_size=output_size)
         return x
+    
+
+class Conv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True, batchnorm=False):
+        super().__init__()
+        self.conv = [nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)]
+
+        if batchnorm:
+            self.conv += [nn.BatchNorm3d(out_channels)]
+
+        self.conv = nn.Sequential(*self.conv)
+
+    def forward(self, x):
+        return self.conv(x)
