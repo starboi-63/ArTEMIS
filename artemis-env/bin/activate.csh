35f8c9fe0bbb5801c8fce844d6e540005044bf67
Tanish Makadia <tanishmakadia@gmail.com>
2024-04-30 20:09:52 -0400

    chore: Configure requirements.txt

--- artemis-env/bin/activate.csh
+++ artemis-env/bin/activate.csh
@@ -1,6 +1,5 @@
 # This file must be used with "source bin/activate.csh" *from csh*.
 # You cannot run it directly.
-
 # Created by Davide Di Blasi <davidedb@gmail.com>.
 # Ported to Python 3.3 venv by Andrew Svetlov <andrew.svetlov@gmail.com>
 
@@ -9,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/kazuyaerdos/programming/ArKTIC-VF/arktic-env"
+setenv VIRTUAL_ENV "/Users/tanishmakadia/Documents/Repositories/ArTEMIS/artemis-env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -18,8 +17,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(arktic-env) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(arktic-env) "
+    set prompt = "(artemis-env) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(artemis-env) "
 endif
 
 alias pydoc python -m pydoc
--- artemis-env/bin/activate.fish
+++ artemis-env/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/). You cannot run it directly.
+# (https://fishshell.com/); you cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kazuyaerdos/programming/ArKTIC-VF/arktic-env"
+set -gx VIRTUAL_ENV "/Users/tanishmakadia/Documents/Repositories/ArTEMIS/artemis-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -56,7 +56,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(arktic-env) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(artemis-env) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -65,5 +65,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(arktic-env) "
+    set -gx VIRTUAL_ENV_PROMPT "(artemis-env) "
 end
