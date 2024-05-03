b0277eef4210ba974c4f3c7fff403ba7d6a10fb2
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-03 17:10:08 -0400

    print all three shapes

--- model/artemis.py
+++ model/artemis.py
@@ -85,9 +85,11 @@ class ArTEMIS(nn.Module):
         dx1 = joinTensors(dx1, x1, type=self.joinType)
 
         low_scale_features = self.smooth1(dx3)
-        print("features shape", low_scale_features.shape)
+        print("low shape", low_scale_features.shape)
         mid_scale_features = self.smooth2(dx2)
+        print("mid shape shape", low_scale_features.shape)
         high_scale_features = self.smooth3(dx1)
+        print("high shape", low_scale_features.shape)
 
         # Generate multiple output frames
         for i in range(1, num_outputs + 1):
