87718863f194101a227d0e551f00ceab99b65418
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-04 18:12:00 -0400

    learning rate scheduler, lots of refactor for lightning

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import metrics
 import shutil
 import os
 
+from lightning.pytorch.loggers import TensorBoardLogger
 import lightning as L
 import torch
 import torch.nn as nn
 
 from model.artemis import ArTEMIS
 from torch.optim import Adamax
+from torch.optim.lr_scheduler import MultiStepLR
 from loss import Loss
 from metrics import eval_metrics
 from data.preprocessing.vimeo90k_septuplet_process import get_loader
@@ -51,8 +53,10 @@ class ArTEMISModel(L.LightningModule):
         self.args = args
         self.model = ArTEMIS(num_inputs=args.nbr_frame, joinType=args.joinType, kernel_size=args.kernel_size, dilation=args.dilation, num_outputs=args.num_outputs)
         self.optimizer = Adamax(self.model.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+        # TODO: SCHEDULE DA LEARNING RATE 
+        # self.scheduler = ... 
         self.loss = Loss(args)
-        self.metrics = eval_metrics
+        self.validation = eval_metrics
 
     def forward(self, images):
         return self.model(images)
@@ -70,14 +74,20 @@ class ArTEMISModel(L.LightningModule):
         images, gt_images = batch
         outputs = self.model(images)
         loss = self.loss(outputs, gt_images)
-        psnr, ssim = self.metrics(outputs, gt_images)
+        psnr, ssim = self.validation(outputs, gt_images)
 
         # log metrics for each step
-        self.log('test_loss', loss)
+        self.log_dict({'test_loss': loss, 'psnr': psnr, 'ssim': ssim})
         
     
     def configure_optimizers(self):
-        return self.optimizer
+        training_schedule = [40, 60, 75, 85, 95, 100]
+        return {
+            "optimizer": self.optimizer,
+            "lr_scheduler": {
+                "scheduler": MultiStepLR(optimizer = self.optimizer, milestones = training_schedule, gamma = 0.5),
+            }
+        }
     
 
 # # call after training
@@ -93,57 +103,6 @@ class ArTEMISModel(L.LightningModule):
 # test_dataloader = DataLoader(dataset)
 # trainer = L.Trainer()
 # trainer.test(model, dataloaders=test_dataloader)
-
-model = ArTEMISModel(args)
-trainer = L.Trainer()
-
-def train(args, epoch):
-
-        # Calc metrics & print logs
-        if i % args.log_iter == 0:
-            for out_image, ground_truth_image in zip(out, gt):
-                metrics.eval_metrics(out_image, ground_truth_image, psnrs, ssims)
-    
-            # Reset metrics
-            losses, psnrs, ssims = metrics.init_meters(args.loss)
-
-
-def test(args, epoch):
-
-    losses, psnrs, ssims = metrics.init_meters(args.loss)
-
-            loss_specific = {
-                'type': loss_specific0['type'],
-                'weight': loss_specific0['weight'],
-                'function': loss_specific0['function']
-            }
-
-            # Save loss values
-            for k, v in losses.items():
-                if k != 'total':
-                    v.update(loss_specific[k].item())
-
-            losses['total'].update(overall_loss.item())
-
-            # Evaluate metrics
-            for out_image, ground_truth_image in zip(out, gt):
-                metrics.eval_metrics(out_image, ground_truth_image, psnrs, ssims)
-            for out_image, ground_truth_image in zip(out, gt):
-                metrics.eval_metrics(out_image, ground_truth_image, psnrs, ssims)
-
-    return losses['total'].avg, psnrs.avg, ssims.avg
-
-
-
-def print_log(epoch, num_epochs, one_epoch_time, oup_pnsr, oup_ssim, Lr):
-    print('({0:.0f}s) Epoch [{1}/{2}], Val_PSNR:{3:.2f}, Val_SSIM:{4:.4f}'
-          .format(one_epoch_time, epoch, num_epochs, oup_pnsr, oup_ssim))
-    # write training log
-    with open('./training_log/train_log.txt', 'a') as f:
-        print(
-            'Date: {0}s, Time_Cost: {1:.0f}s, Epoch: [{2}/{3}], Val_PSNR:{4:.2f}, Val_SSIM:{5:.4f}, Lr:{6}'
-            .format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-                    one_epoch_time, epoch, num_epochs, oup_pnsr, oup_ssim, Lr), file=f)
 
 
 lr_schular = [2e-4, 1e-4, 5e-5, 2.5e-5, 5e-6, 1e-6]
@@ -170,34 +129,47 @@ def main(args):
     # test_loss, psnr, ssim = test(args, args.start_epoch)
     # print(psnr)
 
-    best_psnr = 0
+    # ----------------------------
 
-    for epoch in range(args.start_epoch, args.max_epoch):
-        adjust_learning_rate(optimizer, epoch)
-        start_time = time.time()
-        train(args, epoch)
+    # Train with Lightning 
+    model = ArTEMISModel(args)
+    # logger = TensorBoardLogger("tensorboard_logs", name="ArTEMIS")
+    trainer = L.Trainer(max_epochs = args.max_epoch, log_every_n_steps = args.log_iter)
+    trainer.fit(model, train_loader)
 
-        # save checkpoint
-        torch.save({
-            'epoch': epoch,
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-            'lr': optimizer.param_groups[-1]['lr']
-        }, os.path.join(save_location, 'checkpoint.pth'))
+    # Test the model with Lightning
+    trainer.test(model, test_loader)
 
-        test_loss, psnr, ssim = test(args, epoch)
+    # ----------------------------
 
-        # save checkpoint
-        is_best = psnr > best_psnr
-        best_psnr = max(psnr, best_psnr)
-
-        if is_best:
-            shutil.copyfile(os.path.join(save_location, 'checkpoint.pth'),
-                            os.path.join(save_location, 'model_best.pth'))
-
-        one_epoch_time = time.time() - start_time
-        print_log(epoch, args.max_epoch, one_epoch_time, psnr,
-                  ssim, optimizer.param_groups[-1]['lr'])
+    # best_psnr = 0
+    #
+    # for epoch in range(args.start_epoch, args.max_epoch):
+    #     adjust_learning_rate(optimizer, epoch)
+    #     start_time = time.time()
+    #     train(args, epoch)
+    #
+    #     # save checkpoint
+    #     torch.save({
+    #         'epoch': epoch,
+    #         'state_dict': model.state_dict(),
+    #         'optimizer': optimizer.state_dict(),
+    #         'lr': optimizer.param_groups[-1]['lr']
+    #     }, os.path.join(save_location, 'checkpoint.pth'))
+    #
+    #     test_loss, psnr, ssim = test(args, epoch)
+    #
+    #     # save checkpoint
+    #     is_best = psnr > best_psnr
+    #     best_psnr = max(psnr, best_psnr)
+    #
+    #     if is_best:
+    #         shutil.copyfile(os.path.join(save_location, 'checkpoint.pth'),
+    #                         os.path.join(save_location, 'model_best.pth'))
+    #
+    #     one_epoch_time = time.time() - start_time
+    #     print_log(epoch, args.max_epoch, one_epoch_time, psnr,
+    #               ssim, optimizer.param_groups[-1]['lr'])
 
 
 if __name__ == "__main__":
