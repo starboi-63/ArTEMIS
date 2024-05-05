9a120dc9d7367a5fad67f51dce0b87cbaf2f2eba
Tanish Makadia <tanishmakadia@gmail.com>
2024-05-05 14:27:34 -0400

    refactor: Use PyTorch's built-in loss reduction

--- test_l1_loss.py
+++ test_l1_loss.py
@@ -5,8 +5,8 @@ import torchvision.models as models
 class Loss(nn.modules.loss._Loss):
     def __init__(self, args):
         super(Loss, self).__init__()
-        # just use simple L1 loss. I don't think we need CUDA for this
-        self.loss_function = nn.L1Loss()
+        # just use simple L1 loss.
+        self.loss_function = nn.L1Loss(reduction='mean')
         self.device = torch.device('cuda' if args.cuda else 'cpu')
 
     def forward(self, outputs, ground_truths):
@@ -14,16 +14,11 @@ class Loss(nn.modules.loss._Loss):
         calculate loss for given outputs and ground_truth 
         outputs: tuple of interpolated frames in three lists for ll, l, and output)
         """
-        _, _, out_list = outputs
+        # out_list and ground_truths are [(B,C,H,W), (B,C,H,W), (B,C,H,W)]
+        _, _, out_list = outputs 
 
-        total_loss, num_outputs = 0, 0
+        outputs = torch.cat(out_list, dim=0)            # (B*3, C, H, W)
+        ground_truths = torch.cat(ground_truths, dim=0) # (B*3, C, H, W)
 
-        for out_image, gt_image in zip(out_list, ground_truths):
-            # cuda-ify those images 
-            out_image = out_image.to(self.device)
-            gt_image = gt_image.to(self.device)
-            total_loss += self.loss_function(out_image, gt_image)
-            num_outputs += 1
-
-        return total_loss / num_outputs
+        return self.loss_function(outputs, ground_truths)
 
