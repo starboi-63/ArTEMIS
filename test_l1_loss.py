372cbeed31af0a9cfc0498fc68577db07388561e
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-05 00:24:23 -0400

    clean up l1 loss

--- test_l1_loss.py
+++ test_l1_loss.py
@@ -14,15 +14,16 @@ class Loss(nn.modules.loss._Loss):
         calculate loss for given outputs and ground_truth 
         outputs: tuple of interpolated frames in three lists for ll, l, and output)
         """
-
         _, _, out_list = outputs
 
-        total_loss = 0
+        total_loss, num_outputs = 0, 0
 
         for out_image, gt_image in zip(out_list, ground_truths):
+            # cuda-ify those images 
             out_image = out_image.to(self.device)
             gt_image = gt_image.to(self.device)
             total_loss += self.loss_function(out_image, gt_image)
+            num_outputs += 1
 
-        return total_loss / len(outputs)
+        return total_loss / num_outputs
 
