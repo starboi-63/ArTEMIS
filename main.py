14cd54c01f21c086097b483cc4028ebea17b0fc0
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-06 13:44:08 -0400

    remove DEADBEEF :(

--- main.py
+++ main.py
@@ -158,7 +158,6 @@ def main(args):
     model = ArTEMISModel(args)
     trainer = L.Trainer(max_epochs=args.max_epoch, log_every_n_steps=args.log_iter, logger=logger, enable_checkpointing=args.use_checkpoint)
     print("args use checkpoint", args.use_checkpoint)
-    checkpoint_dir = args.checkpoint_dir if args.use_checkpoint else "./0xDEADBEEF"
 
     # Train with Lightning: Load from checkpoint if specified
     if args.use_checkpoint:
