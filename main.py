3b83e8cc4b31a8c20fba31eb059d55ff4a005f0a
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-05-07 00:09:17 -0400

    AWDLAW

--- main.py
+++ main.py
@@ -134,6 +134,8 @@ class ArTEMISModel(L.LightningModule):
 
         # log metrics for each step
         self.log_dict({'test_loss': loss, 'psnr': psnr, 'ssim': ssim})
+        if batch_idx % args.log_iter == 0:
+            print("psnr", psnr, "ssim", ssim)
         
     
     def configure_optimizers(self):
